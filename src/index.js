671707eb1921f34f407cc82671b9d162e6b1f456
rk-0x0 <rahulkyadav14@gmail.com>
2021-01-28 22:58:59 +0530

    lets try it out

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const myelement = <h1>Pranaam!</h1>
+
+ReactDOM.render( myelement ,  document.getElementById('root'));
+reportWebVitals();
+
+
+/* <React.StrictMode>
+<App />
+</React.StrictMode> */
